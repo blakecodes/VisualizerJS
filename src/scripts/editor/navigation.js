479c52a61b70db4a80bf1ffc0c3df915221436ce
blakecodes <blake@blakecodes.com>
2017-10-26 15:01:45 -0500

    Added change detection and closest parent finding for model building in nav editor

--- src/scripts/editor/navigation.js
+++ src/scripts/editor/navigation.js
@@ -24,42 +24,71 @@ $(function () {
     });
 
 
-
-    // ────────────────────────────────────────────────────────────────────────────────
-
-    //
-    // ─── ATTRIBUTE EDITOR ───────────────────────────────────────────────────────────
-    //
-
-    function generateURL() {
-        return window.location.hostname;
-    }
-
-    // ────────────────────────────────────────────────────────────────────────────────
-
-
     //
     // ─── OBJECT BUILDER ─────────────────────────────────────────────────────────────
     //
 
     class LinkEditor {
         constructor() {
-            this.editor = $('#link-list');
+            var app = this;
+            app.globalIndex = 0;
+            app.editor = $('#link-list');
         }
 
-        buildLinks(links) {
-            links.map(link => {
-                let li = document.createElement('li');
-                let img = document.createElement('img');
-                let span = document.createElement('span');
+        // Get the websites navigation content on initial page load
+        get(callback) {
+            $.ajax({
+                url: '/api/navigation/main.json',
+                type: 'GET'
+            }).done(res => {
+                app.navigation = res;
 
-
-                span.innerHTML = link.text;
+                if (typeof callback === 'function') {
+                    callback();
+                }
             });
         }
 
-        buildLinkEditor() {
+        buildItem(item) {
+            let li = document.createElement('li');
+            let img = document.createElement('img');
+            let span = document.createElement('span');
+            let ul = document.createElement('ul'); // optional
 
+
+            img.src = '/src/assets/icons/path-finder/' + item.type + '.png';
+
+            span.innerHTML = item.pageTitle;
+            span.className = 'nav-label';
+            span.setAttribute('data-nav-index', item.index);
+
+            li.append(img);
+            li.append(span);
+
+            // Add additional section to add files if it's a folder
+            if (item.type === 'folder') {
+                ul.className = 'link-list-sub addNav';
+                li.append(ul);
+            }
+
+            return li;
+        }
+
+        buildLinkEditor(clear = false, callback) {
+            //Clear a section out to reinstate
+            if (clear) {
+                $(clear).html('');
+            }
+
+            setTimeout(function () {
+                app.navigation.map(item => {
+                    app.editor.append(app.buildItem(item));
+                });
+
+                if (typeof callback === 'function') {
+                    callback();
+                }
+            }, 10);
         }
     }
 
@@ -67,27 +96,15 @@ $(function () {
     class NavBuilder extends LinkEditor {
         constructor() {
             super();
+            let app = this;
 
-            var app = this;
-            app.navigation = [];
-
-            this.buildLinkEditor();
-            this.get();
+            this.get(this.buildLinkEditor(false, function () {
+                app.init();
+            }));
         }
 
         init() {
-            this.build();
             this.design();
-        }
-
-        // Get the websites navigation content on initial page load
-        get() {
-            $.ajax({
-                url: '/api/navigation/main.json',
-                type: 'GET'
-            }).done(res => {
-                app.navigation = res;
-            });
         }
 
         //Set a unique identifier to each label
@@ -199,45 +216,83 @@ $(function () {
 
         // Build the html content layout for the navigation
         design() {
+            console.log(app.navigation)
             var navBar = $('#link-holder');
             navBar.html(' ');
             app.navigation.map(nav => {
                 // Create dropdown
                 if (nav.nested.length > 0) {
-                    navBar.append(this.generateDropdown(nav.text, nav.nested));
+                    navBar.append(app.generateDropdown(nav.pageTitle, nav.nested));
                 } else { // Create single level
-                    navBar.append(this.generateBootstrapLink(nav.text));
+                    navBar.append(app.generateBootstrapLink(nav.pageTitle));
                 }
             });
         }
-
-
-        // Develop the full structure of the navigation into an object
-        build() {
-            app.navigation = [];
-
-            this.assing();
-
-            // Add the item to the main object
-            $('#link-list > li > .nav-label').each(function () {
-                let html = $(this).html();
-                let index = $(this).data('nav-index');
-
-                app.navigation.push({
-                    text: html,
-                    index: index,
-                    nested: app.checkDouble(this),
-                    pageTitle: 'test',
-                    pageName: 'test',
-                    pageDescription: 'test',
-                    pageUrl: 'test'
-                });
-            });
-        }
-
     }
 
     // ────────────────────────────────────────────────────────────────────────────────
+
+
+    //
+    // ─── DROP FUNCTIONALITY ─────────────────────────────────────────────────────────
+    //
+    class Page {
+        constructor(index) {
+            this.index = index; // generate dynamic index for item
+            this.nested = false; //default
+            this.pageDescription = 'Page description can go here';
+            this.pageName = 'New Page';
+            this.pageTitle = 'New Page';
+            this.type = 'page';
+        }
+    }
+
+    class Folder {
+        constructor(index) {
+            this.index = index; // generate dynamic index for item
+            this.nested = false; //default
+            this.pageDescription = 'Page description can go here';
+            this.pageName = 'New Page';
+            this.pageTitle = 'New Page';
+            this.type = 'folder';
+        }
+    }
+
+    class DropDetection {
+        constructor() {}
+
+        // Determine type of element that is being added
+        typeDetection(source) {
+            return $(source).data('type');
+        }
+
+        generateIndex() {
+            return 0;
+        }
+
+        // Check to find what index to nest into
+        // This checks the closest parent element with an nav-index assigned.
+        checkNested(target) {
+            return $(target).prev('[data-nav-index]').data('nav-index');
+        }
+
+        // Creates a new object to add to the global scope
+        add(target, source) {
+            let type = this.typeDetection(source);
+
+            var obj = type === 'folder' ? new Folder(this.generateIndex()) : new Page(this.generateIndex());
+
+            var location = this.checkNested(target);
+
+            console.log(location);
+        }
+    }
+
+
+    // ────────────────────────────────────────────────────────────────────────────────
+
+
+
 
 
     //
@@ -251,7 +306,6 @@ $(function () {
 
     // Load model into the form
     function loadModel(model) {
-        console.log('Page Model', model);
         formControl('pageTitle').val(model.pageTitle);
         formControl('pageName').val(model.pageName);
         formControl('pageDescription').val(model.pageDescription);
@@ -265,7 +319,6 @@ $(function () {
 
         var update = $.grep(app.navigation, function (model) {
             if (model.index == index) {
-                console.log(model);
                 model.pageTitle = formControl('pageTitle').val();
                 model.pageName = formControl('pageName').val();
                 model.pageDescription = formControl('pageDescription').val();
@@ -282,10 +335,14 @@ $(function () {
 
     // Run
     var app = new NavBuilder();
-    app.init();
+    var dropDetection = new DropDetection();
+
+
 
     builder.on('drop', function (el, target, source, sibling) {
-        app.init();
+        app.design();
+
+        dropDetection.add(target, el);
     });
 
     $('body').on('click', '.delete-nav', function () {
@@ -304,6 +361,7 @@ $(function () {
                 return e.index == index;
             });
 
+            console.log(result[0]);
             loadModel(result[0]);
         }
     }, '.nav-label');
@@ -315,6 +373,7 @@ $(function () {
             e.preventDefault();
 
             saveModel();
+            app.buildLinkEditor('#link-list', app.design);
         }
     });
 
