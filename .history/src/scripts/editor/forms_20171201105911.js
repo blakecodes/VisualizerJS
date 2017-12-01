c777ff7aaaf4095586da709fbd8581db18cfccfd
blakecodes <blake@blakecodes.com>
2017-12-01 11:06:47 -0600

    Added general structure for Form Builder framework

---
+++ .history/src/scripts/editor/forms_20171201105911.js
@@ -0,0 +1,28 @@
+var FormBuilder = function () {
+
+
+    /**
+     * @namespace Namespace for all conectors
+     */
+    FormBuilder.Extend = $.extend;
+
+
+    /**
+     * Cleans the previous form and initializes a new instance
+     */
+
+
+    /**
+     * Fetches the configuration file for the components form structure
+     * @param {String} componentType Type of component used for fetching form source
+     */
+    FormBuilder.Extend(FormBuilder, {
+
+        /**
+         * @param {String} message console logging message test
+         */
+        testMessage: function (message) {
+            console.log('Testing message');
+        }
+    });
+}
---
+++ src/scripts/editor/forms.js
@@ -0,0 +1,45 @@
+/**
+ * Form Builder V1.0
+ * Designed and developed by Blake Connally under Sequoyah Technologies
+ * This form builder was created to easily intertwine form creation with
+ * AlpacaJS, Bootstrap and jQuery. Initially designed to solve the "no-framework"
+ * event handling with forms. 
+ * 
+ * This frameworks primary functions include form building and data binding. 
+ */
+class FormBuild {
+
+    /**
+     * General configuration implementation
+     * @param {*} config 
+     */
+    constructor(config) {
+
+    }
+
+
+    /**
+     * Clean the entire editing form
+     */
+    completeClean() {
+
+    }
+
+    /**
+     * 
+     * @param {string} componentType 
+     * @param {function} callback 
+     */
+    fetch(componentType, callback) {
+
+    }
+
+    /**
+     * 
+     * @param {form} form submit the entire form to save it's parameters 
+     */
+    submit(form) {
+
+    }
+
+}
