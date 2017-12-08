38cde05437b8a9b37561b81d4565aee8b4692885
blakecodes <blake@blakecodes.com>
2017-12-08 13:01:20 -0600

    Added small style changes to hours of operation

---
+++ src/scripts/Helpers/Slick/slick.js
@@ -0,0 +1,12 @@
+/**
+ * @name SlickBuilder V1.0
+ * @author Blake Connally with Sequoyah Technologies
+ * @version 1.0.0
+ * @summary This is an integration layer for the Slick Scroll library.
+ */
+
+import slick from 'slick-carousel';
+
+class SlickBuilder {
+
+}
